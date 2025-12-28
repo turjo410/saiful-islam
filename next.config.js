93ab63a3dd9cce54dd430a5a448132be2d3eafa7
Shahriar Khan <128829490+ShahriarKhan016@users.noreply.github.com>
2025-12-29 01:31:26 +0600

    Configure Next.js for static export

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/saiful-islam',
+  assetPrefix: '/saiful-islam/',
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'github.com'],
+    unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
